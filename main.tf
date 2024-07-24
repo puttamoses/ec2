598fd0a754aaa7781386fabaed7dcf10a7820d77
Moses putta <136316767+puttamoses@users.noreply.github.com>
2024-07-24 18:25:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,8 @@ resource "aws_instance" "test" {
     instance_type = var.instance_type
     key_name = var.key_name
     associate_public_ip_address = true
-    subnet_id = "subnet-0422bd258abc3a983"
     tags = {
-      Name = "test"
+      Name = "test-server"
     }
     
 }
